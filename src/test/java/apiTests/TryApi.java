21c12c1e716763cd62d2e49b20c327c098208b1d
Artem <somemail2344@gmail.com>
2018-09-25 22:04:01 +0300

    Added the tests for adding the comment to issue and changing of priority in the issue

--- src/test/java/apiTests/TryApi.java
+++ src/test/java/apiTests/TryApi.java
@@ -1,12 +1,11 @@
 package apiTests;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -15,6 +14,8 @@ public class TryApi {
     String username = "webinar5";
     String password = "webinar5";
     String sessionId;
+    String issueId;
+    String commentId;
 
     @BeforeSuite
     public void setupMethod(){
@@ -75,6 +76,99 @@ public class TryApi {
                 then().
                 statusCode(401);
     }
+
+    @Test
+    public void createIssue(){
+        String fieldProject="QAAUT6";
+        String fieldSummary = "API test4";
+        String fieldIssueType = "10105";
+        String fieldAssigneeUser = "Artem Stolbtsov";
+
+        JSONObject project = new JSONObject();
+        JSONObject fields = new JSONObject();
+        JSONObject issueType = new JSONObject();
+        JSONObject assignee = new JSONObject();
+        JSONObject issueCreate = new JSONObject();
+
+        project.put("key", fieldProject);
+        issueType.put("id", fieldIssueType);
+        assignee.put("name", fieldAssigneeUser);
+        fields.put("project",project);
+        fields.put("issuetype", issueType);
+        fields.put("assignee",assignee);
+        fields.put("summary", fieldSummary);
+        issueCreate.put("fields", fields);
+
+        ValidatableResponse response = given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                body(issueCreate.toString()).
+                when().
+                post("/rest/api/2/issue").
+                then().
+                statusCode(201);
+
+        issueId = response.extract().path("id");
+
+    }
+
+    @Test
+    public void addComment(){
+        JSONObject issueComment = new JSONObject();
+
+        issueComment.put("body", "Added comment use to API");
+
+        ValidatableResponse response = given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                body(issueComment.toString()).
+                when().
+                //post("/rest/api/2/issue/"+issueId+"/comment").
+                post("/rest/api/2/issue/34249/comment").
+                then().
+                statusCode(201);
+
+        commentId = response.extract().path("id");
+        Assert.assertEquals(response.extract().path("body"),"Added comment use to API");
+    }
+
+    @Test
+    public void infoIssue(){
+        ValidatableResponse response = given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                when().
+                get("/rest/api/2/issue/34249").then().statusCode(200).log().all();
+    }
+
+    @Test
+    public void updatePriority(){
+        //{"update":{"priority":[{"set":{"id":1}}]}}
+        JSONObject changePriority = new JSONObject();
+        JSONObject update =new JSONObject();
+        JSONArray priority = new JSONArray();
+        JSONObject inArray = new JSONObject();
+        JSONObject set = new JSONObject();
+
+        changePriority.put("update", update);
+        update.put("priority", priority);
+        priority.add(inArray);
+        inArray.put("set", set);
+        set.put("id","1");
+
+        ValidatableResponse response=given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + sessionId).
+                body(changePriority.toString()).
+                when().
+                put("/rest/api/2/issue/34249").
+                then().
+                statusCode(204);
+
+        String responseBody = response.extract().asString();
+    }
+
+
 
 
 
